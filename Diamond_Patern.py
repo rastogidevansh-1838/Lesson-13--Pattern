7baaf33115ceb4289a6046261f69a6b31cbb2fed
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-01-18 22:01:59 +0530

    diamond pattern

---
+++ Diamond_Patern.py
@@ -0,0 +1,13 @@
+rowsize=int(input("Enter the number of rows: "))
+if rowsize%2==0:
+    halfdiamrow=int(rowsize/2)
+else:
+    halfdiamrow=int(rowsize/2)+1
+space = halfdiamrow-1
+for i in range(1, halfdiamrow+1):
+    for j in range(1, space+1):
+        print(end=" ")
+    space = space-1
+    num=1
+    for j in range(2*i-1):
+        for i in range
